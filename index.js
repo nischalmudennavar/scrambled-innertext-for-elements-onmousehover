ea5ae422f05dafe220cfefe602f6079fe99098ac
Nischal Jagadish Mudennavar <nischalmudennavar@gmail.com>
2023-02-26 13:54:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const buttons = document.querySelectorAll("div");
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+}{|:?><~`-=[];',./";
+
+let interval = null;
+
+buttons.forEach((button) => {
+  button.addEventListener("mouseover", (event) => {
+    let iteration = 0;
+
+    clearInterval(interval);
+
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if (index < iteration) {
+            return event.target.dataset.value[index];
+          }
+
+          return letters[Math.floor(Math.random() * letters.length)];
+        })
+        .join("");
+
+      if (iteration > event.target.dataset.value.length) {
+        clearInterval(interval);
+      }
+
+        iteration += 1 / 2;
+        
+    }, 30);
+  });
+});
+
+buttons.forEach((button) => {
+    button.addEventListener("mouseleave", (event) => {
+        clearInterval(interval);
+        event.target.innerText = event.target.dataset.value;
+    });
+    });
